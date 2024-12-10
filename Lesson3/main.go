ff04baf0a0994c425af967ddb5cc6735e8805c08
Ryan Hoang <anhoang800140@gmail.com>
2024-12-10 11:38:22 +0700

    Declare variable type

---
+++ Lesson3/main.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+var c, python, java bool
+
+func main() {
+	var i int = 2 //declare and initialize a variable, can outside a function
+	k := 3        // short variable declaration, only inside a function
+	fmt.Println(i, c, python, java)
+	fmt.Println(k)
+}
