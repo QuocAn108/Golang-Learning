f8e9b0e9c11fef42799a1c8ad98637178339081d
Ryan Hoang <anhoang800140@gmail.com>
2024-12-25 23:52:28 +0700

    Function using map, not using generic

--- Lesson32/main.go
+++ Lesson32/main.go
@@ -1,8 +1,19 @@
 package main
 
-type List[T any] struct {
-	next  *List[T]
-	value T
+import "fmt"
+
+func mapInt(arr []int, f func(int) int) []int {
+	result := make([]int, len(arr))
+	for i, v := range arr {
+		result[i] = f(v)
+	}
+	return result
 }
 
-func main() {}
+func main() {
+	arr := []int{1, 2, 3, 4, 5}
+	result := mapInt(arr, func(v int) int {
+		return v * 2
+	})
+	fmt.Println(result)
+}
