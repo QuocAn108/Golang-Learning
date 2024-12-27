134f173caab80067dc4f24b3423ec4dffaa04aa2
Ryan Hoang <anhoang800140@gmail.com>
2024-12-27 22:27:51 +0700

    Add note

--- Lesson35/main.go
+++ Lesson35/main.go
@@ -3,7 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	ch := make(chan int, 2)
+	ch := make(chan int, 2) // buffered channel with capacity 2, don't need to use goroutine or func to receive the value from the channel
 	ch <- 1
 	ch <- 2
 	fmt.Println(<-ch)
