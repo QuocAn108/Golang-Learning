d5e2a920ca3cb3ed251eb08d8d5158376dfa4e54
Ryan Hoang <anhoang800140@gmail.com>
2024-12-28 20:27:08 +0700

    Note for range and close

--- Lesson36/main.go
+++ Lesson36/main.go
@@ -18,3 +18,6 @@ func main() {
 		fmt.Println(i)
 	}
 }
+
+//Channels aren't like files; you don't usually need to close them. Closing is only necessary when the receiver must be told there are no more values coming, such as to terminate a range loop.
+//Note: Only the sender should close a channel, never the receiver. Sending on a closed channel will cause a panic.
