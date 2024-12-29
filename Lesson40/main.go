780ebf06de3a0b5be4131430d7e3e91b6dff4555
Ryan Hoang <anhoang800140@gmail.com>
2024-12-29 21:25:44 +0700

    Adding  waitgroup replace for time.sleep

--- Lesson40/main.go
+++ Lesson40/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"sync"
 	"time"
 )
 
@@ -9,15 +10,24 @@ func main() {
 	n := 10000
 	maxWorker := 5
 	queueCh := make(chan int, n)
+	wg := new(sync.WaitGroup)
 	for i := 1; i <= n; i++ {
 		queueCh <- i
 	}
 	close(queueCh)
 	for i := 0; i <= maxWorker; i++ {
+		wg.Add(1)
 		go func(count int) {
-			time.Sleep(time.Second)
-			fmt.Printf("Worker %d is crawling web url %d\n", count, <-queueCh)
+			for v := range queueCh {
+				time.Sleep(time.Millisecond * 20)
+				fmt.Printf("Worker %d is crawling web url %d\n", count, v)
+			}
+			wg.Done()
 		}(i)
 	}
-	time.Sleep(time.Second * 100)
+	wg.Wait()
 }
+
+//using waitgroup to wait for all goroutines to finish before exiting the main function
+//using channel to pass values to goroutines
+//using close to close the channel
